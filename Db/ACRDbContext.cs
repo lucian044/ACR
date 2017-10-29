1c2cda311a3c34e82fc98bea37ec384112bfaec5
lucian044 <lbm21@zips.uakron.edu>
2017-10-29 16:08:39 -0400

    database migration

--- Db/ACRDbContext.cs
+++ Db/ACRDbContext.cs
@@ -8,6 +8,11 @@ namespace Db
     {
         public DbSet<Employee> Employee { get; set; }
         public DbSet<School> School { get; set; }
+        public DbSet<Week> Week { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<Holiday> Holiday { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Benefit> Benefit { get; set; }
 
         public ACRDbContext(DbContextOptions<ACRDbContext> options) : base(options)
         { }
@@ -18,6 +23,11 @@ namespace Db
             // Remove pluralizing
             modelBuilder.Entity<Employee>().ToTable("Employee");
             modelBuilder.Entity<School>().ToTable("School");
+            modelBuilder.Entity<Week>().ToTable("Week");
+            modelBuilder.Entity<Role>().ToTable("Role");
+            modelBuilder.Entity<Holiday>().ToTable("Holiday");
+            modelBuilder.Entity<Category>().ToTable("Category");
+            modelBuilder.Entity<Benefit>().ToTable("Benefit");
         }
     }
 }
--- Migrations/20171029200611_InitialSchema.Designer.cs
+++ Migrations/20171029200611_InitialSchema.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ACR.Migrations
 {
     [DbContext(typeof(ACRDbContext))]
-    partial class ACRDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171029200611_InitialSchema")]
+    partial class InitialSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
